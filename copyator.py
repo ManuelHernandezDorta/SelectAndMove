edda77aaede120ccbe556e14a6acd23e5dbc9696
Manuel Hernández Dorta <70766797+ManuelHernandezDorta@users.noreply.github.com>
2021-03-08 13:03:24 +0000

    Create copyator.py

---
+++ copyator.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--destination", "-d", help="to set output path")
+parser.add_argument("--file", "-f", help="set the path of input file list")
+parser.add_argument("--location", "-l", help="set the path of the target directory (default is corrent working directory)")
+parser.add_argument("--extension", "-e", help="adds extension to input file name (write it without .)")
+args = parser.parse_args()
+
+directory = os.getcwd()
+array = []
+extension = ""
+
+if args.destination:
+    destination = args.destination
+if args.file:
+    paper = args.file
+if args.location:
+    directory = args.location
+if args.extension:
+    extension = "."+args.extension
+
+with open(paper,"r") as names:
+    for name in names.read().splitlines():
+        array.append(name + extension)
+    names.close()
+
+if os.path.isdir(destination):
+    pass
+else:
+    os.mkdir(destination)
+
+for position in array:
+    for document in os.listdir(directory):
+        if position == document:
+            shutil.copyfile(document,destination)
+        else:
+            pass
